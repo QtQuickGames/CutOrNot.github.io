31849ada5a8e70645bd438feb1fc22724e62b95f
QtQuickGames <78654153+QtQuickGames@users.noreply.github.com>
2023-12-02 12:13:19 +0100

    corrected map location

--- script.js
+++ script.js
@@ -72,8 +72,8 @@ let iterator = 1;
 var markers = new L.MarkerClusterGroup({});
 data = await collectPinData('geojson/drzewa4p.geojson');
 data.forEach(element=>{
-  let dat = L.marker([element.geometry.coordinates[1],element.geometry.coordinates[0]], {icon:treeIcon});
-dat.bindTooltip(`<b>id</b>: ${iterator} <br> <b>koordynaty</b>: ${element.geometry.coordinates[1].toFixed(2)}, ${element.geometry.coordinates[0].toFixed(2)}`);
+  let dat = L.marker([element.geometry.coordinates[1] / 10000, element.geometry.coordinates[0] / 10000], {icon:treeIcon});
+dat.bindTooltip(`<b>id</b>: ${iterator} <br> <b>koordynaty</b>: ${element.geometry.coordinates[1].toFixed(2) / 10000}, ${element.geometry.coordinates[0].toFixed(2) / 10000}`);
 iterator++;
   markers.addLayer(dat);
 });
