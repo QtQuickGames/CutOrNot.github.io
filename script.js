f767547631b7eec510287d5d30519b507ad8ecc3
QtQuickGames <78654153+QtQuickGames@users.noreply.github.com>
2023-12-04 18:14:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,10 +70,10 @@ var lodz = L.polygon(element, {color: 'green'}).addTo(map);
 
 let iterator = 1;
 var markers = new L.MarkerClusterGroup({});
-data = await collectPinData('geojson/drzewa4p.geojson');
+data = await collectPinData('geojson/Drzewa_WGS84.geojson');
 data.forEach(element=>{
-  let dat = L.marker([element.geometry.coordinates[1] / 10000, element.geometry.coordinates[0] / 10000], {icon:treeIcon});
-dat.bindTooltip(`<b>id</b>: ${iterator} <br> <b>koordynaty</b>: ${element.geometry.coordinates[1].toFixed(2) / 10000}, ${element.geometry.coordinates[0].toFixed(2) / 10000}`);
+  let dat = L.marker([element.geometry.coordinates[1], element.geometry.coordinates[0]], {icon:treeIcon});
+dat.bindTooltip(`<b>id</b>: ${iterator} <br> <b>koordynaty</b>: ${element.geometry.coordinates[1].toFixed(2)}, ${element.geometry.coordinates[0].toFixed(2)`);
 iterator++;
   markers.addLayer(dat);
 });
